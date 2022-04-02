b727112c155eb94ddb20e4e7f2215d3184819055
gaelle kerforne <stage.seeweb@gmail.com>
2022-04-02 20:48:10 +0200

    ajout de nouvelle entity dans le user et gestions de la validation du formulaire

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,20 +4,41 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\ResgiterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordHashes): Response
     {
         $user = new User();
         $form = $this->createForm(ResgiterType::class, $user);
+        // ecoute la requete
 
-        return $this->render('register/index.html.twig',[
+        $form->handleRequest($request);
+        // est-ce que mon formulaire a ete soumis et est ce que mon formulaire est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // tu inject dans l'objet user toutes le données que tu recupere dans le formulaire
+            $user = $form->getData();
+            dump($user);
+
+            // hash du mdp
+            $password = $passwordHashes->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+            dump($password);
+
+            // enregistrement en bdd
+            $manager->persist($user); // je prepare et je fige la donner pour la creation de l'entity
+            $manager->flush();
+        }
+        return $this->render('register/index.html.twig', [
             'form' => $form->createView()
-            ]);
+        ]);
     }
+
 }
